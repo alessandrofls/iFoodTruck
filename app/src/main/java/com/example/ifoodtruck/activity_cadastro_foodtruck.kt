c5969e3f8b72d5660be93c6eb750ea6ed5388ec0
Alessandro <alessandrolimabcc@gmail.com>
2018-11-08 14:57:16 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/ifoodtruck/activity_cadastro_foodtruck.kt
@@ -0,0 +1,12 @@
+package com.example.ifoodtruck
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_cadastro_foodtruck : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_foodtruck)
+    }
+}
