09030bf476e198573cc21e140e3e8fe2fdae08c4
Alessandro <alessandrolimabcc@gmail.com>
2018-11-12 15:32:17 -0300

    Criação da tela de cadastro e Drawerlayout

---
+++ app/src/main/java/com/example/ifoodtruck/DrawerLayout.kt
@@ -0,0 +1,12 @@
+package com.example.ifoodtruck
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DrawerLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drawer_layout)
+    }
+}
