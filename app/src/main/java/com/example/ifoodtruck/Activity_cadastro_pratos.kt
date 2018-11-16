dd9281b6fb39b3d22bf296437de671a74a2bc34c
Viviane Araujo <viviane.baraujo1997@gmail.com>
2018-11-16 02:27:59 -0300

    tela adm e user

---
+++ app/src/main/java/com/example/ifoodtruck/Activity_cadastro_pratos.kt
@@ -0,0 +1,12 @@
+package com.example.ifoodtruck
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_cadastro_pratos : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_pratos)
+    }
+}
---
+++ app/src/main/java/com/example/ifoodtruck/TelaAdm.kt
@@ -0,0 +1,12 @@
+package com.example.ifoodtruck
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaAdm : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_adm)
+    }
+}
