fb90729a5d55f04b34eed5318a9d9f5bfeaab258
Viviane Araujo <viviane.baraujo1997@gmail.com>
2019-01-05 16:34:20 -0300

    sign in feito e inicio de sign up

--- app/src/main/java/com/example/ifoodtruck/Login.kt
+++ app/src/main/java/com/example/ifoodtruck/Login.kt
@@ -3,21 +3,48 @@ package com.example.ifoodtruck
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
+
+    var mAuth : FirebaseAuth? = null
+    var mAuthListening : FirebaseAuth.AuthStateListener? = null
+    var int = getIntent()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-            var int = getIntent()
 
-        btentrar.setOnClickListener(){
-            val intTA = Intent (this, TelaAdm::class.java)
-            startActivity(intTA)
-        }
+
+        mAuth= FirebaseAuth.getInstance()
+        mAuthListening = FirebaseAuth.AuthStateListener { }
+
+
+//        btentrar.setOnClickListener(){
+//            val intTA = Intent (this, TelaAdm::class.java)
+//            startActivity(intTA)
+//        }
 
 
     }
+
+    fun signin (view: View){
+        mAuth!!.signInWithEmailAndPassword(userLogin.text.toString(),userSenha.text.toString())
+
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        val intTA = Intent (this, TelaAdm::class.java)
+                        startActivity(intTA)
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(this, R.string.MensagemErro, Toast.LENGTH_SHORT).show()
+                }
+    }
+
+
 }
--- app/src/main/java/com/example/ifoodtruck/activity_cadastro_foodtruck.kt
+++ app/src/main/java/com/example/ifoodtruck/activity_cadastro_foodtruck.kt
@@ -1,11 +1,27 @@
 package com.example.ifoodtruck
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_cadastro_foodtruck.*
+import kotlinx.android.synthetic.main.activity_login.*
+import java.util.*
 
 class activity_cadastro_foodtruck : AppCompatActivity() {
+
+    var mAuth: FirebaseAuth? = null
+    var firebaseDatabase: FirebaseDatabase? = null
+    var dbRef: DatabaseReference? = null
+    var mStorageRef: StorageReference? = null
+    var selected: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +29,13 @@ class activity_cadastro_foodtruck : AppCompatActivity() {
 
         var int = getIntent()
 
+        mAuth = FirebaseAuth.getInstance()
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        dbRef = firebaseDatabase!!.reference
+        mStorageRef = FirebaseStorage.getInstance().reference
+
         btncadastrar.setOnClickListener(){
+
             Toast.makeText(this, R.string.messagemcadastrar, Toast.LENGTH_SHORT).show()
 
         }
@@ -22,5 +44,30 @@ class activity_cadastro_foodtruck : AppCompatActivity() {
 
        }
 
+    }
+    fun signup (view: View){
+
+        val uuid = UUID.randomUUID()
+        val imageName = "images/$uuid.jpg"
+
+        val storageReference = mStorageRef!!.child(imageName)
+
+        var uploadTask = storageReference.putFile(selected!!)
+
+        val uuidString = uuid.toString()
+
+        mAuth!!.createUserWithEmailAndPassword(eTLogin.text.toString(),eTSenha.text.toString())
+
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        //add outros campos
+                        dbRef!!.child("FoodTruck").child(uuidString).child("nomeEstabelecimento").setValue(edtNomeEstab.text.toString())
+                        Toast.makeText(this, R.string.messagemcadastrar, Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(this, R.string.MensagemErro, Toast.LENGTH_SHORT).show()
+                }
     }
 }
