fd68727e86f0fa27f4be6886bdac9c8b089d6d30
Viviane Araujo <viviane.baraujo1997@gmail.com>
2019-01-08 06:04:54 -0300

    cadastro de estab com database

--- app/src/main/java/com/example/ifoodtruck/Beans/FoodTruck.kt
+++ app/src/main/java/com/example/ifoodtruck/Beans/FoodTruck.kt
@@ -2,12 +2,13 @@ package com.example.ifoodtruck.Beans
 
 class FoodTruck( val nomeEstabelecimento: String,
                  val NomeProprietario:String,
-                 val telefone: Int,
+                 val telefone: String,
                  val endereco:String ,
                  val especialidade:String,
                  val senha:String,
                  val confirmacaoSenha:String ,
-                 val login:String ){
+                 val login:String,
+                 val foto:String){
                //  val comentarios: ArrayList<String>? ) {
 
 }
--- app/src/main/java/com/example/ifoodtruck/activity_cadastro_foodtruck.kt
+++ app/src/main/java/com/example/ifoodtruck/activity_cadastro_foodtruck.kt
@@ -1,9 +1,15 @@
 package com.example.ifoodtruck
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.annotation.RequiresApi
+import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -14,6 +20,8 @@ import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_cadastro_foodtruck.*
 import kotlinx.android.synthetic.main.activity_login.*
 import java.util.*
+import android.Manifest
+import android.support.v4.app.ActivityCompat
 
 class activity_cadastro_foodtruck : AppCompatActivity() {
 
@@ -34,21 +42,21 @@ class activity_cadastro_foodtruck : AppCompatActivity() {
         dbRef = firebaseDatabase!!.reference
         mStorageRef = FirebaseStorage.getInstance().reference
 
-        btncadastrar.setOnClickListener(){
+//        btncadastrar.setOnClickListener(){
+//
+//            Toast.makeText(this, R.string.messagemcadastrar, Toast.LENGTH_SHORT).show()
+//
+//        }
 
-            Toast.makeText(this, R.string.messagemcadastrar, Toast.LENGTH_SHORT).show()
-
-        }
-
-       btncancelar.setOnClickListener(){
-
-       }
+//       btncancelar.setOnClickListener(){
+//
+//       }
 
     }
     fun signup (view: View){
 
         val uuid = UUID.randomUUID()
-        val imageName = "images/$uuid.jpg"
+        val imageName = "ImagensFoodTrucks/$uuid.jpg"
 
         val storageReference = mStorageRef!!.child(imageName)
 
@@ -56,18 +64,86 @@ class activity_cadastro_foodtruck : AppCompatActivity() {
 
         val uuidString = uuid.toString()
 
-        mAuth!!.createUserWithEmailAndPassword(eTLogin.text.toString(),eTSenha.text.toString())
 
-                .addOnCompleteListener { task ->
-                    if(task.isSuccessful){
-                        //add outros campos
-                        dbRef!!.child("FoodTruck").child(uuidString).child("nomeEstabelecimento").setValue(edtNomeEstab.text.toString())
+        if (eTSenha.text.toString().equals(eTConfirmaSenha.text.toString()) && cbAccept.isChecked){
+
+            mAuth!!.createUserWithEmailAndPassword(eTLogin.text.toString(),eTSenha.text.toString())
+
+                uploadTask.addOnSuccessListener { taskSnapshot ->
+
+                            dbRef!!.child("FoodTruck").child(uuidString).child("nomeEstabelecimento").setValue(edtNomeEstab.text.toString())
+                            dbRef!!.child("FoodTruck").child(uuidString).child("NomeProprietario").setValue(eTNomeProprietario.text.toString())
+                            dbRef!!.child("FoodTruck").child(uuidString).child("telefone").setValue(eTTelefone.text.toString())
+                            dbRef!!.child("FoodTruck").child(uuidString).child("endereco").setValue(eTcadEndereco.text.toString())
+                            dbRef!!.child("FoodTruck").child(uuidString).child("especialidade").setValue(eTEspecialidade.text.toString())
+                            dbRef!!.child("FoodTruck").child(uuidString).child("login").setValue(eTLogin.text.toString())
+                            dbRef!!.child("FoodTruck").child(uuidString).child("senha").setValue(eTSenha.text.toString())
+
+
+
+                        storageReference.getDownloadUrl().addOnSuccessListener { uri ->
+                            dbRef!!.child("FoodTruck").child(uuidString).child("foto").setValue(uri.toString())
+                        }
+                }
+
+                uploadTask.addOnCompleteListener { task ->
+                    if (task.isComplete) {
                         Toast.makeText(this, R.string.messagemcadastrar, Toast.LENGTH_SHORT).show()
 
                     }
                 }
-                .addOnFailureListener { exception ->
-                    Toast.makeText(this, R.string.MensagemErro, Toast.LENGTH_SHORT).show()
-                }
+                    .addOnFailureListener { exception ->
+                        Toast.makeText(this, R.string.MensagemErro, Toast.LENGTH_SHORT).show()
+                    }
+
+        }
+
+
     }
+
+
+    fun selectImage(view: View) {
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        } else {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, 2)
+        }
+
+    }
+
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        if (requestCode == 1) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent, 2)
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK && data != null) {
+            selected = data.data
+
+            try {
+
+                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selected)
+                imageView.setImageBitmap(bitmap)
+
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
